32c8ea45ea4f75c9585287ec385467371d3ac7e2
Michele Silva <michele.silva@gmail.com>
2013-02-11 17:54:48 -0200

    Tells superuser when receiving messages from somebody else

--- BitlyBot.py
+++ BitlyBot.py
@@ -33,6 +33,7 @@ class BitlyBot(GtalkRobot):
         super(BitlyBot, self, *args, **kwargs).__init__()
         self.bitly_bundle = None 
         self.bitly_connection = None
+        self.superuser = None
 
 
     @bot_command('(http[s]*://.+$)')
@@ -44,21 +45,20 @@ class BitlyBot(GtalkRobot):
         :param args: the list of parameters as matched on the regex
         """
         print "{0} entered a link: {1}.".format(user, args[0])
+        self.inform_superuser(user, message)
         try:
             if self.bitly_connection:
-
                 # Either save it on the declared bundle or not into any bundle
                 if self.bitly_bundle:
                     self.bitly_connection.bundle_link_add(self.bitly_bundle, args[0])
                 else:
                     self.bitly_connection.user_link_save(args[0])
-
                 self.replyMessage(user, "Your link just got eaten by the pufferfish!")
             else:
                 self.replyMessage(user, "The pufferfish is not around!")
 
         except bitly_api.BitlyError as error:
-            print error
+            print 'Bitly Error:', error
             self.replyMessage(user, "Sorry, the pufferfish is already full!")
 
 
@@ -71,9 +71,24 @@ class BitlyBot(GtalkRobot):
         :param args: the list of parameters as matched on the regex
         """
 	print user, "said: {0}".format(message)
+        self.inform_superuser(user, message)
+
 	self.replyMessage(user, "I save links starting with http " + 
             "or https to bitly ({0}). ".format(self.bitly_bundle) +
             "Try entering a URL, such as http://bit.ly")
+
+
+    def inform_superuser(self, user, message):
+        """ Tell superuser when someone else sends a message to the bot
+
+        :param user: the email address of the user requesting the command
+        :param message: the text as entered by the user
+        """
+        user = user.getStripped() 
+        if self.superuser and user != self.superuser:
+	    self.replyMessage(self.superuser, "{0} said {1}".format(
+                user, message))
+
 
 
 def main():
@@ -88,6 +103,7 @@ def main():
     bot = BitlyBot()
     bot.setState('available', 'I will save your links to bitly')
 
+    bot.superuser = super_email
     bot.bitly_connection = bitly_api.Connection(
         access_token=bitly_access_token)
     bot.bitly_bundle = bitly_bundle
